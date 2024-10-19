4d7643504acb0168fc657aad7356a539e81f5353
kabir4874 <kabir.cse.bd@gmail.com>
2024-10-19 20:49:08 +0600

    update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,9 @@
 import bcrypt from "bcryptjs";
 import asyncHandler from "../middlewares/asyncHandler.js";
 import User from "../models/userModel.js";
-const createUser = asyncHandler(async (req, res) => {
+import createToken from "../utils/createToken.js";
+
+export const createUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
   if (!username || !email || !password) {
     throw new Error("All fields are required");
@@ -13,10 +15,23 @@ const createUser = asyncHandler(async (req, res) => {
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res.status(201).json({ newUser });
+    createToken(res, newUser._id);
+    res.status(201).json({ newUser, message: "User created successfully" });
   } catch (error) {
     res.status(400);
     throw new Error("Invalid user data");
   }
 });
-export { createUser };
+
+export const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) res.status(400).send("User not found");
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    res.status(400).send("Password don't match");
+  }
+  createToken(res, user._id);
+  res.status(201).json({ message: "Login successfully" });
+});
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { createUser } from "../controllers/userController.js";
+import { createUser, loginUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.post("/", createUser);
-
+router.post("/signup", createUser);
+router.post("/login", loginUser);
 export default router;
